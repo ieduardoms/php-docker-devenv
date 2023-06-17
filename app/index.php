e2c63fafbd55a7572e160626a4aa0a8031e781e9
Eduardo Moreno <git config --global user.email>
2023-06-17 11:22:51 +0800

    Create repository

---
+++ app/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $myName = "Write your name here.";
+        echo "<p>Hi, $myName</p>";
+        echo "<p>Hello World! I am PHP.</p>";
+        phpinfo();
+        xdebug_info();
+    ?>
+</body>
+</html>
